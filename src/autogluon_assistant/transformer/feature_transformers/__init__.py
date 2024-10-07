ad50241247710dda7f65866e525ac720c3c7d965
boranhan <boranhan.dl@gmail.com>
2024-10-07 14:35:12 -0700

    added GloVe trained embedding.

--- src/autogluon_assistant/transformer/feature_transformers/__init__.py
+++ src/autogluon_assistant/transformer/feature_transformers/__init__.py
@@ -1,9 +1,11 @@
 from .base import BaseFeatureTransformer
 from .caafe import CAAFETransformer
 from .openfe import OpenFETransformer
+from .glove import GloveTextEmbeddingTransformer
 
 __all__ = [
     "BaseFeatureTransformer",
     "CAAFETransformer",
     "OpenFETransformer",
+    "GloveTextEmbeddingTransformer",
 ]
--- src/autogluon_assistant/transformer/feature_transformers/base.py
+++ src/autogluon_assistant/transformer/feature_transformers/base.py
@@ -31,7 +31,7 @@ class BaseFeatureTransformer(BaseTransformer):
                 train_y=train_y,
                 target_column_name=task.label_column,
                 problem_type=task.problem_type,
-                dataset_description=task.meatadata["description"],
+                dataset_description=task.data_description,
             )
         except TransformTimeoutError:
             logger.warning(f"FeatureTransformer {self.__class__.__name__} timed out.")
@@ -45,28 +45,28 @@ class BaseFeatureTransformer(BaseTransformer):
         raise NotImplementedError
 
     def transform(self, task: TabularPredictionTask) -> TabularPredictionTask:
-        try:
-            train_x = task.train_data.drop(
-                columns=task.columns_in_train_but_not_test + [task.test_id_column],
-                errors="ignore",
+        #try:
+        train_x = task.train_data.drop(
+            columns=task.columns_in_train_but_not_test + [task.test_id_column],
+            errors="ignore",
+        )
+        train_y = task.train_data[task.label_column]
+        test_x = task.test_data.drop(columns=[task.test_id_column])
+
+        train_x, test_x = self._transform_dataframes(train_X=train_x, test_X=test_x)
+
+        # add back id and label columns
+        transformed_train_data = pd.concat([train_x, train_y.rename(task.label_column)], axis=1)
+        if task.test_id_column in task.train_data.columns:
+            transformed_train_data = pd.concat(
+                [transformed_train_data, task.train_data[task.test_id_column]], axis=1
             )
-            train_y = task.train_data[task.label_column]
-            test_x = task.test_data.drop(columns=[task.test_id_column])
+        transformed_test_data = pd.concat([test_x, task.test_data[task.test_id_column]], axis=1)
 
-            train_x, test_x = self._transform_dataframes(train_X=train_x, test_X=test_x)
-
-            # add back id and label columns
-            transformed_train_data = pd.concat([train_x, train_y.rename(task.label_column)], axis=1)
-            if task.test_id_column in task.train_data.columns:
-                transformed_train_data = pd.concat(
-                    [transformed_train_data, task.train_data[task.test_id_column]], axis=1
-                )
-            transformed_test_data = pd.concat([test_x, task.test_data[task.test_id_column]], axis=1)
-
-            task = copy.deepcopy(task)
-            task.train_data = transformed_train_data
-            task.test_data = transformed_test_data
-        except:
-            logger.warning(f"FeatureTransformer {self.__class__.__name__} failed to transform.")
-        finally:
-            return task
+        task = copy.deepcopy(task)
+        task.train_data = transformed_train_data
+        task.test_data = transformed_test_data
+        #except:
+            #logger.warning(f"FeatureTransformer {self.__class__.__name__} failed to transform.")
+        #finally:
+        return task
